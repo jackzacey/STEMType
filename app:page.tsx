289e899f95836490c66a16c856a44f47723a172a
jackzacey <jackzacey@gmail.com>
2025-11-22 21:50:03 -0800

    Add files via upload
    initial site

---
+++ app:page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+export default function Home() {
+  return (
+    
+STEMType
+Boost your typing speed while mastering Biology & Chemistry terms
+Info Start Learning →
+); }
+
