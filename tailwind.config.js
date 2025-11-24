c1f4492b9a819043a8f3f1686e4e5ba8410f8567
jackzacey <jackzacey@gmail.com>
2025-11-23 21:19:34 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config}
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -11,9 +11,9 @@ module.exports = {
         card: "#1e293b",
         blue: "#64d2ff",
         gold: "#facc15",
-        text: "#e2e8f0",
-      },
-    },
+        text: "#e2e8f0"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
