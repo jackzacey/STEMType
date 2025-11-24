fc9ca4a36607559dab4bdc686bd2047c6c97a2ff
jackzacey <jackzacey@gmail.com>
2025-11-23 21:35:36 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
-    config.resolve.alias['@'] = '.';
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,   // ← this is the correct way
+    };
     return config;
   },
 };
