52f65ed1e1eecca4b3b3109910a973db679c181f
jackzacey <jackzacey@gmail.com>
2025-11-23 21:45:22 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,16 @@ export const metadata: Metadata = {
   description: 'Master Biology & Chemistry by typing',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body className="bg-bg text-text min-h-screen">{children}</body>
+      <body className="bg-bg text-text min-h-screen antialiased">
+        {children}
+      </body>
     </html>
   )
 }
