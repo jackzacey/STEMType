d165cd8ee75e5ce4fb9078a9eb9e808c76d7e1e1
jackzacey <jackzacey@gmail.com>
2025-11-23 21:24:26 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.resolve.alias['@'] = '.';
+    return config;
+  },
+};
 
 export default nextConfig;
