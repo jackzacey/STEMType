7ca507311aa1a6133c9f0c825cae03a419b6e0a6
jackzacey <jackzacey@gmail.com>
2025-11-23 21:37:47 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 // next.config.mjs
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': __dirname,   // ← this is the correct way
+      '@': __dirname,
     };
     return config;
   },
