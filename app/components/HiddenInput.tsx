757c3df7209ffdfe11ea27b21a3f86b6185854a5
jackzacey <jackzacey@gmail.com>
2025-11-24 19:01:03 -0800

    Create HiddenInput.tsx

---
+++ app/components/HiddenInput.tsx
@@ -0,0 +1,18 @@
+// app/components/HiddenInput.tsx
+'use client';
+
+import { forwardRef } from 'react';
+
+export const HiddenInput = forwardRef<HTMLInputElement>((props, ref) => (
+  <input
+    ref={ref}
+    type="text"
+    autoFocus
+    aria-hidden="true"
+    className="fixed -left-[999999px] top-[999999px] w-0 h-0 opacity-0 outline-none border-none -z-50"
+    style={{ caretColor: 'transparent', pointerEvents: 'none' }}
+    {...props}
+  />
+));
+
+HiddenInput.displayName = 'HiddenInput';
