47fe4c1d8667c7053ec06f0def897eba88697dbb
jackzacey <jackzacey@gmail.com>
2025-11-24 19:11:50 -0800

    Update HiddenInput.tsx

--- app/components/HiddenInput.tsx
+++ app/components/HiddenInput.tsx
@@ -9,8 +9,12 @@ export const HiddenInput = forwardRef<HTMLInputElement>((props, ref) => (
     type="text"
     autoFocus
     aria-hidden="true"
-    className="fixed -left-[999999px] top-[999999px] w-0 h-0 opacity-0 outline-none border-none -z-50"
-    style={{ caretColor: 'transparent', pointerEvents: 'none' }}
+    className="fixed left-[-999999px] top-[-999999px] w-0 h-0 opacity-0 outline-none border-none -z-50"
+    style={{
+      caretColor: 'transparent',
+      pointerEvents: 'none',
+      transform: 'translateX(-999999px) translateY(-999999px)',
+    }}
     {...props}
   />
 ));
