75141422466a15b2175ca49b2d7d952487c19799
jackzacey <jackzacey@gmail.com>
2025-11-24 20:22:38 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
-// app/layout.tsx
 import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: 'STEMTYPE',
-  description: 'Type the Universe',
 }
 
 export default function RootLayout({
@@ -14,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body> {/* no background at all */}
+      <body className="m-0 p-0">{children}</body>
     </html>
   )
 }
